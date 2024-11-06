50df82cdda7134a0207cb129fb6a91f386cde13b
Codaea <dakotajayroth@gmail.com>
2024-11-05 22:45:18 -0800

    more UI updates

--- pages/online/answer/sent.vue
+++ pages/online/answer/sent.vue
@@ -1,6 +1,19 @@
 <template>
-
-    <div>
-        <h1>Are you actually right though?...</h1>
+  <div class="min-h-screen flex items-center justify-center">
+    <div class="space-y-4">
+      <p class="text-7xl">{{ randomSaying }}</p>
+      <p class="text-xl text-center">Waiting for round to end...</p>
+      <!-- LOADING SVG HERE -->
     </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+const sayings = [
+  'Best guesser ever?',
+  "You didn't cheat did you?",
+  'Good Guess!',
+]
+
+const randomSaying = sayings[Math.floor(Math.random() * sayings.length)]
+</script>
--- pages/online/gameblock.vue
+++ pages/online/gameblock.vue
@@ -1,57 +1,60 @@
 <template>
   <div>
-  <h1>Guess the location!</h1>
-  <UModal v-model="isGuessOpen">
-    <UCard :ui="{body: { padding: '', }}">
-      <template #header>
-        <h1 class="text-3xl text-center">Guess Where!</h1>
-      </template>
-      <div class="h-[32rem] w-full">
-        <GuessMap @update:guess="updateGuess" />
-      </div>
-      <template #footer>
-        <UButton @click="isGuessOpen = false">Close</UButton>
-        <UButton :disabled="guess == null" @click="submit" >Guess</UButton>
-      </template>
-    </UCard>
-  </UModal> 
-  
-  <UButton
-    class="fixed bottom-14 left-1/2 transform -translate-x-1/2 z-10"
-    size="xl"
-    @click="isGuessOpen = !isGuessOpen"
-    >Guess</UButton
-  >
+    <UModal v-model="isGuessOpen">
+      <UCard :ui="{ body: { padding: '' } }">
+        <template #header>
+          <h1 class="text-3xl text-center">Guess!</h1>
+        </template>
+        <div class="h-[60vh] max-h-[32rem] w-full overflow-auto">
+          <GuessMap @update:guess="updateGuess" />
+        </div>
+        <template #footer>
+          <div class="flex flex-row justify-between items-center w-full">
+            <UButton @click="isGuessOpen = false">Close</UButton>
+            <UButton :disabled="guess == null" @click="submit">Submit</UButton>
+          </div>
+        </template>
+      </UCard>
+    </UModal>
 
-  <VideoPlayer class="fixed top-0 left-0 -z-10 w-screen h-screen pointer-events-none select-none" :video-id="videoId" />
+    <UButton
+      class="fixed bottom-14 left-1/2 transform -translate-x-1/2 z-10"
+      size="xl"
+      @click="isGuessOpen = !isGuessOpen"
+      >Guess</UButton
+    >
 
+    <VideoPlayer
+      class="fixed top-0 left-0 -z-10 w-screen h-screen pointer-events-none select-none"
+      :video-id="videoId"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Coordinates } from '~/types/Coordinates'
-import { useGameState } from '~/compostables/useGameState';
-import { getSocket } from '~/components/socket';
+import { useGameState } from '~/compostables/useGameState'
+import { getSocket } from '~/components/socket'
 
-const gameState = useGameState();
-const socket = getSocket();
+const gameState = useGameState()
+const socket = getSocket()
 
-const videoId: Ref<string> = gameState.videoId;
-const isGuessOpen = ref(false);
+const videoId: Ref<string> = gameState.videoId
+const isGuessOpen = ref(false)
 
-const guess = ref<Coordinates|null>(null);
-const router = useRouter();
+const guess = ref<Coordinates | null>(null)
+const router = useRouter()
 
-function updateGuess(value: Coordinates):void {
-  guess.value = value;
+function updateGuess(value: Coordinates): void {
+  guess.value = value
 }
 
 function submit() {
-  console.log(`Guessing: ${guess.value}`);
-  socket.emit('guess', guess.value); 
-  router.push('/answer/sent');
+  if (guess.value) {
+    gameState.setGuess(guess.value);
+    console.log(`Guessing: ${guess.value}`)
+    socket.emit('guess', guess.value)
+    router.push('/online/answer/sent')
+  }
 }
-
-
-
 </script>
