6354cee9ec01e951e16a3506975c519eb952c54f
Codaea <dakotajayroth@gmail.com>
2024-11-19 13:58:02 -0800

    added pinia for state managment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/leaflet', '@nuxt/eslint'],
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/leaflet',
+    '@nuxt/eslint',
+    '@pinia/nuxt'
+  ],
   nitro: {
     experimental: {
       websocket: true
